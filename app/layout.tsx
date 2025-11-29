6dd52e3c1fc859730f89f4ebb157f0d77d77400f
MateHUNAa <you@example.com>
2025-11-29 06:41:45 +0100

    style: Add server logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,22 @@ import { Layout, Navbar } from "nextra-theme-docs";
 import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
+import Image from "next/image";
 
 export const metadata = {};
 
-const navbar = <Navbar logo={<b>Nextra</b>} />;
+const navbar = (
+     <Navbar
+          logo={
+               <div className="flex flex-row gap-2 items-center justify-center">
+                    <Image src={"/logo.gif"} alt="PLS Logo" width={32} height={32} />
+                    <span>
+                         <b>Project | Los Santos</b>
+                    </span>
+               </div>
+          }
+     />
+);
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
      return (
