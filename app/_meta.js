b01d2e0baa40eedbdd9ea38b7780fad01fdf6e73
Gergő <69343477+Rav3n95@users.noreply.github.com>
2025-12-04 19:03:42 +0100

    A faszom az egeszbe

--- app/_meta.js
+++ app/_meta.js
@@ -1,12 +1,10 @@
 export default {
      core: "Core",
-     logger: "Logger",
      audio: "Audio",
      peds: "Peds",
      status: "Status",
      ui: "UI",
      skilltree: "Skilltree",
-
      keybinds: "Keybinds",
      blips: "Blips",
      routing: "Routing",
