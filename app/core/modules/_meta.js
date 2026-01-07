ca0687ef0a4053d3121d394cb286e4deb9e7dab9
Gergő <69343477+Rav3n95@users.noreply.github.com>
2026-01-07 10:16:28 +0100

    Core: Update

--- app/core/modules/_meta.js
+++ app/core/modules/_meta.js
@@ -1,4 +1,13 @@
 export default {
   anim: 'Anim',
   blip: 'Blip',
+  callback: 'Callback',
+  camera: 'Camera',
+  ui: 'UI',
+  dui: 'DUI',
+  proximity: 'Proximity (getClosest/getNearby)',
+  streaming: 'Streaming (Request modulok)',
+  utilities: 'Utilities (Logger, Math, Table, WaitFor)',
+  'game-systems': 'Game Systems (Zones, Raycast, Keybinds, Cron)',
+  misc: 'Egyéb (GetRelativeCoords, GetFilesInDirectory)'
 }
